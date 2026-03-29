f0b35d87d0a7224e541ceb1debe8fa7e22455e85
angel b <angelica.bracamonte.data24hel@edu.tucsweden.se>
2026-03-29 20:08:12 +0200

    init

---
+++ src/main/java/se/blug/blug_forum/BlugForumApplication.java
@@ -0,0 +1,13 @@
+package se.blug.blug_forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlugForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlugForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/blug/blug_forum/BlugForumApplicationTests.java
@@ -0,0 +1,13 @@
+package se.blug.blug_forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlugForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
